f0575ac384d2e7273d34dbe40087db6f63242198
linyangofmay <linyangofmay@gmail.com>
2022-07-06 14:57:03 -0700

    title

--- public/index.js
+++ public/index.js
@@ -1,1 +1,22 @@
 // Your code here
+const catLoader = async()=>{
+
+ const title = document.createElement('h1');
+ title.innerText = 'Kitten Pic';
+ document.body.appendChild(title);
+
+ const container = document.createElement()
+
+
+
+
+
+
+
+
+
+
+}
+
+
+window.onload = catLoader;
