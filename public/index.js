9ca1a362b97a7e1bd9656786f05f0b05d9732c4d
linyangofmay <linyangofmay@gmail.com>
2022-07-06 15:18:33 -0700

    pic

--- public/index.js
+++ public/index.js
@@ -5,7 +5,17 @@ const catLoader = async()=>{
  title.innerText = 'Kitten Pic';
  document.body.appendChild(title);
 
- const container = document.createElement()
+ const container = document.createElement('div');
+ const catImg = await fetch('https://api.thecatapi.com/v1/images/search?size=small');
+ console.log('catImg', catImg);
+ const cats = await catImg.json();
+ console.log('cats', cats);
+
+ const img = document.createElement('img');
+ const url = cats[0].url;
+ img.setAttribute('src', url);
+ container.appendChild(img);
+ document.body.appendChild(container);
 
 
 
